cbd223e1f502938940d01118f2e54d7f41bac4f4
Millz98 <dave_m86@hotmail.com>
2015-02-14 22:02:26 -0600

    this is different

--- src/com/base/engine/Texture.java
+++ src/com/base/engine/Texture.java
@@ -3,7 +3,7 @@ package com.base.engine;
 
 import static org.lwjgl.opengl.GL11.*;
 
-public class Texture;
+public class Texture
 {
     private int id;
     
@@ -16,25 +16,13 @@ public class Texture;
     {
         return id;
     }
-}
 
-public class Texture 
-{
-    private int id; 
-	
-	public Texture(int id)
-     {
-    	 this.id = id;
-     }
-	
-	public void bind()
+
+    public void bind()
 	{
 		glBindTexture(GL_TEXTURE_2D, id);
 	}
 	
-	public int getID()
-	{
-		return id;
-	}
+	
 }
 
