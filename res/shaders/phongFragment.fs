bfd5017f2ce0e9b7b69f38f1411c2da57d1473b7
Millz98 <dave_m86@hotmail.com>
2015-11-19 20:53:11 -0600

    lighting system so far...

--- res/shaders/phongFragment.fs
+++ res/shaders/phongFragment.fs
@@ -1,6 +1,7 @@
 #version 430
 
 in vec2 texCoord0;
+in vec3 normal0;
 
 out vec4 fragColor;
 
@@ -8,7 +9,42 @@ uniform vec3 baseColor;
 uniform vec3 ambientLight;
 uniform sampler2D sampler;
 
-void main()
+struct BaseLight
+{
+   vec3 color;
+   float intensity;
+
+ };
+ 
+ struct DirectionalLight
+ {
+      BaseLight base;
+	  vec3 direction;
+ };
+ //CalcLight is to calculate where the light is coming from
+ vec4 calcLight(BaseLight base, vec3 direction, vec3 normal)
+ {
+    float diffuseFactor = dot(-direction, normal);
+	
+	vec4 diffuseColor = vec4(0,0,0,0);
+	
+	if(diffuseFactor > 0)
+	(
+	    diffuseColor = vec4(base.color, 1.0) * base.intensity * diffuseFactor;
+	)
+	
+	return diffuseColor;
+ }
+ 
+ vec4 calcDirectionalLight(DirectionalLight directionalLight, vec3 normal)
+ 
+ (
+    return calcLight(directionalLight.base, directionalLight.direction, normal); 
+ )
+ 
+
+ 
+ void main()
 {
     vec4 totalLight = vec4 (ambientLight,1);
     vec4 color = vec4(baseColor, 1);
