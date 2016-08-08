26bb0b0a984822f0c6081756fe53d08a48669187
Millz98 <dave_m86@hotmail.com>
2016-08-07 23:24:18 -0600

    Point Light implementation

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -7,12 +7,13 @@ public class Game
 	private Transform transform;
 	private Camera camera;
 	
-	
+	PointLight pLight1 = new PointLight(new BaseLight(new Vector3f(1,0.5f,0), 0.8f), new Attenuation(0,0,1), new Vector3f(-2,0,5f));
+	PointLight pLight2 = new PointLight(new BaseLight(new Vector3f(0,0.5f,1), 0.8f), new Attenuation(0,0,1), new Vector3f(2,0,7f));
 	
 	public Game()
 	{
 		mesh = new Mesh(); //ResourceLoader.loadMesh("box.obj")
-		material = new Material(ResourceLoader.loadTexture("test.png"), new Vector3f(1,1,1));
+		material = new Material(ResourceLoader.loadTexture("test.png"), new Vector3f(1,1,1), 1, 8);
 	    shader = PhongShader.getInstance();
 		camera = new Camera();
 		transform = new Transform();
@@ -28,28 +29,43 @@ public class Game
 //				                    0,1,2,
 //				                    0,2,3};
 		
-		Vertex[] verticies = new Vertex[] { new Vertex( new Vector3f(-1.0f, -1.0f, 0.5773f),	new Vector2f(0.0f, 0.0f)),
-				        new Vertex( new Vector3f(0.0f, -1.0f, -1.15475f),		new Vector2f(0.5f, 0.0f)),
-				        new Vertex( new Vector3f(1.0f, -1.0f, 0.5773f),		new Vector2f(1.0f, 0.0f)),
-				        new Vertex( new Vector3f(0.0f, 1.0f, 0.0f),      new Vector2f(0.5f, 1.0f)) };
+		//Vertex[] verticies = new Vertex[] { new Vertex( new Vector3f(-1.0f, -1.0f, 0.5773f),	new Vector2f(0.0f, 0.0f)),
+				       // new Vertex( new Vector3f(0.0f, -1.0f, -1.15475f),		new Vector2f(0.5f, 0.0f)),
+				       // new Vertex( new Vector3f(1.0f, -1.0f, 0.5773f),		new Vector2f(1.0f, 0.0f)),
+				       // new Vertex( new Vector3f(0.0f, 1.0f, 0.0f),      new Vector2f(0.5f, 1.0f)) };
 				
-						int indicies[] = { 0, 3, 1,
-						1, 3, 2,
-						2, 3, 0,
-						1, 2, 0 };
+						//int indicies[] = { 0, 3, 1,
+						//1, 3, 2,
+						//2, 3, 0,
+						//1, 2, 0 };
+		        
+		        float fieldDepth = 10.0f;
+				float fieldWidth = 10.0f;
+				
+				Vertex[] vertices = new Vertex[] { 	new Vertex( new Vector3f(-fieldWidth, 0.0f, -fieldDepth), new Vector2f(0.0f, 0.0f)),
+													new Vertex( new Vector3f(-fieldWidth, 0.0f, fieldDepth * 3), new Vector2f(0.0f, 1.0f)),
+													new Vertex( new Vector3f(fieldWidth * 3, 0.0f, -fieldDepth), new Vector2f(1.0f, 0.0f)),
+													new Vertex( new Vector3f(fieldWidth * 3, 0.0f, fieldDepth * 3), new Vector2f(1.0f, 1.0f))};
+				
+				int indices[] = { 0, 1, 2,
+							      2, 1, 3};
 		
 		
 		
-		
-		mesh.addVertices(verticies, indicies, true);
+		mesh.addVertices(vertices, indices, true);
 		
 		Transform.setProjection(70f, Window.getWidth(), Window.getHeight(), 0.1f, 1000);
 		Transform.setCamera(camera);
 		
 		// the line below is what sets the light of a surface if the light isn't directly hitting it
 		PhongShader.setAmbientLight(new Vector3f(0.1f,0.1f,0.1f));
-		PhongShader.setDirectionalLight(new DirectionalLight(new BaseLight(new Vector3f(1,1,1), 0.8f), new Vector3f(1,1,1)));
+		//PhongShader.setDirectionalLight(new DirectionalLight(new BaseLight(new Vector3f(1,1,1), 0.8f), new Vector3f(1,1,1)));
 		
+		// code below is setting the pointlights that we set up in the PointLight.java class and the phoneshader.java class
+		
+		
+		
+		PhongShader.setPointLight(new PointLight[]{pLight1, pLight2});
 		
 	}
 	
@@ -80,8 +96,12 @@ public class Game
 		
 		float sinTemp = (float)Math.sin(temp);
 		
-		transform.setTranslation(sinTemp, 0, 5);
-		transform.setRotation(0, sinTemp * 180, 0);
+		transform.setTranslation(sinTemp, -1, 5);
+		//transform.setRotation(0, sinTemp * 180, 0);
+		
+		pLight1.setPosition(new Vector3f(3,0,8.0f * (float)(Math.sin(temp) + 1.0/2.0) + 10));
+		pLight2.setPosition(new Vector3f(7,0,8.0f * (float)(Math.cos(temp) + 1.0/2.0) + 10));
+		
 		//transform.setScale(0.7f * sinTemp,0.7f * sinTemp,0.7f * sinTemp);
 	}
 	
--- src/com/base/engine/PhongShader.java
+++ src/com/base/engine/PhongShader.java
@@ -2,7 +2,9 @@ package com.base.engine;
 
 public class PhongShader extends Shader
 {
-private static final PhongShader instance = new PhongShader();
+	private static final int MAX_POINT_LIGHTS = 4;
+	
+    private static final PhongShader instance = new PhongShader();
 	
 	public static PhongShader getInstance()
 	{
@@ -11,6 +13,8 @@ private static final PhongShader instance = new PhongShader();
 	
 	private static Vector3f ambientLight = new Vector3f(0.1f,0.1f,0.1f);
 	private static DirectionalLight directionalLight = new DirectionalLight(new BaseLight(new Vector3f(1,1,1), 0), new Vector3f(0,0,0));
+	private static PointLight[] pointLights = new PointLight[] {}; 
+	
 	
 	private PhongShader()
 	{
@@ -32,7 +36,17 @@ private static final PhongShader instance = new PhongShader();
 		addUniform("directionalLight.base.color");
 		addUniform("directionalLight.base.intensity");
 		addUniform("directionalLight.direction");
-
+		
+		for(int i = 0; i < MAX_POINT_LIGHTS; i++)
+		{
+			addUniform("pointLights[" + i + "].base.color");
+			addUniform("pointLights[" + i + "].base.intensity");
+			addUniform("pointLights[" + i + "].atten.constant");
+			addUniform("pointLights[" + i + "].atten.linear");
+			addUniform("pointLights[" + i + "].atten.exponent");
+			addUniform("pointLights[" + i + "].position");
+		}
+        // Above code "should" initialize every point light uniform.
 	}
 	
 	public void updateUniforms(Matrix4f worldMatrix, Matrix4f projectedMatrix, Material material)
@@ -48,6 +62,9 @@ private static final PhongShader instance = new PhongShader();
 		
 		setUniform("ambientLight", ambientLight);
 		setUniform("directionalLight", directionalLight);
+		
+		for(int i = 0; i < pointLights.length; i++)
+			setUniform("pointLights[" + i + "]", pointLights[i]);
 		
 		setUniformf("specularIntensity", material.getSpecularIntensity());
 		setUniformf("specularPower", material.getSpecularPower());
@@ -70,6 +87,18 @@ private static final PhongShader instance = new PhongShader();
 		PhongShader.directionalLight = directionalLight;
 	}
 	
+	public static void setPointLight(PointLight[] pointLights)
+	{
+		if(pointLights.length > MAX_POINT_LIGHTS)
+		{
+			System.err.println("Error: You passed in too many point lights! Max allowed is " + MAX_POINT_LIGHTS + ", you passed in " + pointLights.length);
+			new Exception().printStackTrace();
+			System.exit(1);
+		}
+		
+		PhongShader.pointLights = pointLights;
+	}
+	
 	public void setUniform(String uniformName, BaseLight baseLight)
 	{
 		setUniform(uniformName + ".color", baseLight.getColor());
@@ -80,6 +109,16 @@ private static final PhongShader instance = new PhongShader();
 	{
 		setUniform(uniformName + ".base", directionalLight.getBase());
 		setUniform(uniformName + ".direction", directionalLight.getDirection());
+	}
+	
+	public void setUniform(String uniformName, PointLight pointLight)
+	{
+		setUniform(uniformName + ".base", pointLight.getBaseLight());
+		setUniformf(uniformName + ".atten.constant", pointLight.getAtten().getConstant());
+		setUniformf(uniformName + ".atten.linear", pointLight.getAtten().getLinear());
+		setUniformf(uniformName + ".atten.exponent", pointLight.getAtten().getExponent());
+		setUniform(uniformName + ".position", pointLight.getPosition());
+		
 	}
 
 }
