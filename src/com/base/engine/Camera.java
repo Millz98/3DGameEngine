36c6682c690b8727a6a7ae3daac0b95c9c85ef2b
Millz98 <dave_m86@hotmail.com>
2024-05-19 18:38:35 -0600

    camera small fix, can't build yet

--- src/com/base/engine/Camera.java
+++ src/com/base/engine/Camera.java
@@ -1,148 +1,126 @@
 package com.base.engine;
 
-public class Camera 
-{
-	public static final Vector3f yAxis = new Vector3f (0,1,0); //this lets me know the ABSOLUTE up in the world. 
-	
+public class Camera {
+	public static final Vector3f yAxis = new Vector3f(0, 1, 0); // this lets me know the ABSOLUTE up in the world.
+
 	private Vector3f pos;
 	private Vector3f forward;
 	private Vector3f up;
-	
-	public Camera()
-	{
-		this(new Vector3f(0,0,0), new Vector3f(0,0,1), new Vector3f(0,1,0));
+
+	public Camera() {
+		this(new Vector3f(0, 0, 0), new Vector3f(0, 0, 1), new Vector3f(0, 1, 0));
 	}
-	
-	public Camera(Vector3f pos, Vector3f forward, Vector3f up)
-	{
-		
+
+	public Camera(Vector3f pos, Vector3f forward, Vector3f up) {
+
 		this.pos = pos;
-        
-        this.forward = forward.normalized();
+
+		this.forward = forward.normalized();
 		this.up = up.normalized();
 	}
-    
-    boolean mouseLocked = false;
-	Vector2f centerPosition = new Vector2f(Window.getWidth()/2, Window.getHeight()/2);
-	
-	public void input()
-	{
+
+	boolean mouseLocked = false;
+	Vector2f centerPosition = new Vector2f(Window.getWidth() / 2, Window.getHeight() / 2);
+
+	public void input() {
 		float sensitivity = 0.5f;
-        float movAmt = (float)(10 * Time.getDelta());
-        //float rotAmt = (float)(100 * Time.getDelta());
-        
-        if(Input.getKey(Input.KEY_ESCAPE))
-		{
+		float movAmt = (float) (10 * Time.getDelta());
+		// float rotAmt = (float)(100 * Time.getDelta());
+
+		if (Input.getKey(Input.KEY_ESCAPE)) {
 			Input.setCursor(true);
 			mouseLocked = false;
 		}
-		if(Input.getMouseDown(0))
-		{
+		if (Input.getMouseDown(0)) {
 			Input.setMousePosition(centerPosition);
 			Input.setCursor(false);
 			mouseLocked = true;
 		}
-        
-        if(Input.getKey(Input.KEY_W))
-            move(getForward(), movAmt);
-        if(Input.getKey(Input.KEY_S))
-            move(getForward(), -movAmt);
-        if(Input.getKey(Input.KEY_A))
-            move(getLeft(), movAmt);
-        if(Input.getKey(Input.KEY_D))
-            move(getRight(), movAmt);
-        
-        if(mouseLocked)
-        {
-            Vector2f deltaPos = Input.getMousePosition().sub(centerPosition);
-            
-            boolean rotY = deltaPos.getX() != 0;
-            boolean rotX = deltaPos.getY() != 0;
-            
-            if(rotY)
-                rotateY(deltaPos.getX() * sensitivity);
-            if(rotX)
-                rotateX(-deltaPos.getY() * sensitivity);
-            
-            if(rotY || rotX)
-                Input.setMousePosition(new Vector2f(Window.getWidth()/2, Window.getHeight()/2));
-            
-        }
-            
-        // these keys below were to enable me to rotate the camera around the object with the arrow keys
-//         if(Input.getKey(Input.KEY_UP))
-//             rotateX(-rotAmt);
-//         if(Input.getKey(Input.KEY_DOWN))
-//             rotateX(rotAmt);
-//         if(Input.getKey(Input.KEY_LEFT))
-//             rotateY(-rotAmt);
-//         if(Input.getKey(Input.KEY_RIGHT))
-//             rotateY(rotAmt);
+
+		if (Input.getKey(Input.KEY_W))
+			move(getForward(), movAmt);
+		if (Input.getKey(Input.KEY_S))
+			move(getForward(), -movAmt);
+		if (Input.getKey(Input.KEY_A))
+			move(getLeft(), movAmt);
+		if (Input.getKey(Input.KEY_D))
+			move(getRight(), movAmt);
+
+		if (mouseLocked) {
+			Vector2f deltaPos = Input.getMousePosition().sub(centerPosition);
+
+			boolean rotY = deltaPos.getX() != 0;
+			boolean rotX = deltaPos.getY() != 0;
+
+			if (rotY)
+				rotateY(deltaPos.getX() * sensitivity);
+			if (rotX)
+				rotateX(-deltaPos.getY() * sensitivity);
+
+			if (rotY || rotX)
+				Input.setMousePosition(new Vector2f(Window.getWidth() / 2, Window.getHeight() / 2));
+
+		}
+
+		// these keys below were to enable me to rotate the camera around the object
+		// with the arrow keys
+		// if(Input.getKey(Input.KEY_UP))
+		// rotateX(-rotAmt);
+		// if(Input.getKey(Input.KEY_DOWN))
+		// rotateX(rotAmt);
+		// if(Input.getKey(Input.KEY_LEFT))
+		// rotateY(-rotAmt);
+		// if(Input.getKey(Input.KEY_RIGHT))
+		// rotateY(rotAmt);
 	}
-    
-    
-	
-	// the code right below this comment is what enables the camera to be moved. 
-	public void move(Vector3f dir, float amt)
-	{
-		pos= pos.add(dir.mul(amt));
+
+	// the code right below this comment is what enables the camera to be moved.
+	public void move(Vector3f dir, float amt) {
+		pos = pos.add(dir.mul(amt));
 	}
-	
-	public void rotateY(float angle)
-	{
+
+	public void rotateY(float angle) {
 		Vector3f Haxis = yAxis.cross(forward).normalized();
-		
+
 		forward = forward.rotate(angle, yAxis).normalized();
-		
+
 		up = forward.cross(Haxis).normalized();
 	}
-	
-	public void rotateX(float angle)
-	{
-		Vector3f Haxis = yAxis.cross(forward).normalized();
-		
-		forward = forward.rotate(angle, Haxis).normalized();
-		
-		up = forward.cross(Haxis).normalized();
+
+	public void rotateX(float angle) {
+		forward = forward.rotate(angle, up).normalized();
+		up = forward.cross(yAxis).normalized();
 	}
-	
-	public Vector3f getLeft()
-	{
+
+	public Vector3f getLeft() {
 		return forward.cross(up).normalized();
 	}
-	
-	public Vector3f getRight()
-	{
+
+	public Vector3f getRight() {
 		return up.cross(forward).normalized();
 	}
 
-	public Vector3f getPos() 
-	{
+	public Vector3f getPos() {
 		return pos;
 	}
 
-	public void setPos(Vector3f pos) 
-	{
+	public void setPos(Vector3f pos) {
 		this.pos = pos;
 	}
 
-	public Vector3f getForward() 
-	{
+	public Vector3f getForward() {
 		return forward;
 	}
 
-	public void setForward(Vector3f forward) 
-	{
+	public void setForward(Vector3f forward) {
 		this.forward = forward;
 	}
 
-	public Vector3f getUp() 
-	{
+	public Vector3f getUp() {
 		return up;
 	}
 
-	public void setUp(Vector3f up) 
-	{
+	public void setUp(Vector3f up) {
 		this.up = up;
-	}  
+	}
 }
